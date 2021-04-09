8be1003fca2e87227a240a2f1a79f92d4194736b
Derivolcov Mihail <mderivolcov@mail.ru>
2021-04-09 13:55:21 +0300

    Last mod 09.04.2021

---
+++ src/GildedRose.Console/GildenRose.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console
+{
+    public static class GildenRose
+    {
+        public static void UpdateQuality(IList<Item> Items)
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if((Items[i].Name == "Aged Brie") 
+                || (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if(Items[i].SellIn > 0)
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality++;
+                        }
+
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality++;
+                            }
+                        }
+
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            Items[i].Quality = 0;
+                        }
+                    }
+                }
+                else if(Items[i].Name == "Conjured Mana Cake")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        Items[i].Quality -= 2;
+                        if(Items[i].Quality < 0) 
+                        { 
+                            Items[i].Quality = 0; 
+                        }
+                    }
+                }
+                else if(Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    if(Items[i].Quality > 0)
+                    {
+                        Items[i].Quality--;
+                    }     
+                }
+
+                if ((Items[i].SellIn < 0) && (Items[i].Quality > 0))
+                {
+                    Items[i].Quality--;
+                }
+
+                Items[i].SellIn--;
+            }
+        }
+    }
+}
--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -4,132 +4,54 @@ namespace GildedRose.Console
 {
     class Program
     {
-        public class Item
-        {
-            public string Name { get; set; }
-            public int SellIn { get; set; }
-            public int Quality { get; set; }
-        }
-        IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
+            IList<Item> Items = new List<Item>
             {
-                Items = new List<Item>
+                new Item 
                 {
-                    new Item 
-                    {
-                        Name = "+5 Dexterity Vest", 
-                        SellIn = 10, 
-                        Quality = 20
-                    },
-                    new Item 
-                    {
-                        Name = "Aged Brie", 
-                        SellIn = 2, 
-                        Quality = 0
-                    },
-                    new Item 
-                    {
-                        Name = "Elixir of the Mongoose", 
-                        SellIn = 5, 
-                        Quality = 7
-                    },
-                    new Item 
-                    {
-                        Name = "Sulfuras, Hand of Ragnaros", 
-                        SellIn = 0, 
-                        Quality = 80
-                    },
-                    new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 15,
-                        Quality = 20
-                    },
-                    new Item 
-                    {
-                        Name = "Conjured Mana Cake", 
-                        SellIn = 3, 
-                        Quality = 6
-                    }
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 10, 
+                    Quality = 20
+                },
+                new Item 
+                {
+                    Name = "Aged Brie", 
+                    SellIn = 2, 
+                    Quality = 0
+                },
+                new Item 
+                {
+                    Name = "Elixir of the Mongoose", 
+                    SellIn = 5, 
+                    Quality = 7
+                },
+                new Item 
+                {
+                    Name = "Sulfuras, Hand of Ragnaros", 
+                    SellIn = 0, 
+                    Quality = 80
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item 
+                {
+                    Name = "Conjured Mana Cake", 
+                    SellIn = 3, 
+                    Quality = 6
                 }
             };
 
-            app.UpdateQuality();
+            GildenRose.UpdateQuality(Items);
 
             System.Console.ReadKey();
-        }
-
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if ((Items[i].Name != "Aged Brie") 
-                  &&(Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                  &&(Items[i].Name != "Sulfuras, Hand of Ragnaros"))
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        Items[i].Quality--;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality++; 
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if ((Items[i].SellIn < 11) && (Items[i].Quality < 50))
-                            {
-                                Items[i].Quality++;
-                            }
-
-                            if ((Items[i].SellIn < 6) && (Items[i].Quality < 50))
-                            {
-                                Items[i].Quality++;
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn--; 
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality--;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = 0;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality++;
-                        }
-                    }
-                }
-            }
-        }
+        } 
     }
 }
+
